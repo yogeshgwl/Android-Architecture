e80ebba49f7148b36aef68aa7cfb05c518baea34
Shubham.Bhawsar <shubham.bhawsar@galaxyweblinks.in>
2022-06-07 18:52:37 +0530

    Feedback changes

--- app/src/androidTest/java/com/task/TestUtil.kt
+++ app/src/androidTest/java/com/task/TestUtil.kt
@@ -2,16 +2,19 @@ package com.task
 
 import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import com.task.data.dto.recipes.Recipes
 import com.task.data.dto.recipes.RecipesItem
 import java.io.InputStream
-import java.lang.reflect.Type
 
 object TestUtil {
     var dataStatus: DataStatus = DataStatus.Success
     var recipes: Recipes = Recipes(arrayListOf())
     fun initData(): Recipes {
-        return Recipes(arrayListOf())
+        val jsonString = getJson("RecipesApiResponse.json")
+        val theList = GsonBuilder().create().fromJson<ArrayList<RecipesItem>>(jsonString, object : TypeToken<ArrayList<RecipesItem>>(){}.type)
+        return Recipes(theList)
     }
 
     private fun getJson(path: String): String {
--- app/src/main/java/com/task/data/dto/recipes/RecipesItem.kt
+++ app/src/main/java/com/task/data/dto/recipes/RecipesItem.kt
@@ -6,58 +6,33 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class RecipesItem(
-        @SerializedName("calories")
         val calories: String = "",
-        @SerializedName("carbos")
         val carbos: String = "",
-        @SerializedName("card")
         val card: String = "",
-        @SerializedName("country")
         val country: String = "",
         @SerializedName("deliverable_ingredients")
         val deliverableIngredients: List<String> = listOf(),
-        @SerializedName("description")
         val description: String = "",
-        @SerializedName("difficulty")
         val difficulty: Int = 0,
-        @SerializedName("fats")
         val fats: String = "",
-        @SerializedName("favorites")
         val favorites: Int = 0,
-        @SerializedName("fibers")
         val fibers: String = "",
-        @SerializedName("headline")
         val headline: String = "",
-        @SerializedName("highlighted")
         val highlighted: Boolean = false,
-        @SerializedName("id")
         val id: String = "",
-        @SerializedName("image")
         val image: String = "",
-        @SerializedName("incompatibilities")
         val incompatibilities: String = "",
-        @SerializedName("ingredients")
         val ingredients: List<String> = listOf(),
-        @SerializedName("keywords")
         val keywords: List<String> = listOf(),
-        @SerializedName("name")
         val name: String = "",
-        @SerializedName("products")
         val products: List<String> = listOf(),
-        @SerializedName("proteins")
         val proteins: String = "",
-        @SerializedName("rating")
         val rating: Double = 0.0,
-        @SerializedName("ratings")
         val ratings: Int = 0,
-        @SerializedName("thumb")
         val thumb: String = "",
-        @SerializedName("time")
         val time: String = "",
         @SerializedName("undeliverable_ingredients")
         val undeliverableIngredients: List<String> = listOf(),
-        @SerializedName("user")
         val user: User = User(),
-        @SerializedName("weeks")
         val weeks: List<String> = listOf()
 ) : Parcelable
